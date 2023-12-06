689f898d5ab7ac136e36d316c6cbcb0739e83bd2
장백이안 <6011311@gmail.com>
2023-12-07 03:23:15 +0800

    更新 github-private-repo.js

---
+++ js/github-private-repo.js
@@ -0,0 +1,31 @@
+/**
+允许引用Github私有仓库/gist中的订阅分流，重写，脚本等配置文件。❗️仅Loon
+@author: 易安
+🛎[配置说明]
+0️⃣ 
+使用Loon插件:
+https://raw.githubusercontent.com/zbaiicn/master/Plugin/github-private-repo.plugin
+1️⃣ 登陆Github > 点击头像下拉菜单 > 选择Settings > 左边菜单栏选择最后一个Developer settings > 选择Personal access tokens > Generate new token > Note里面填写token名字 > ☑️下面的勾选框选择第一项repo打钩（所有子项目自动勾选）> 点击Generate token按钮保存并返回。
+2️⃣ 在BoxJS里面填入用户名（打开Github，浏览器地址栏应该会显示https://github.com/这里是你的用户名/）和上面的token。
+*/
+
+// 如果不使用BoxJS配置，可以在这里修改
+let config = {
+  username: "zbaiicn", // 用户名
+  token: "Your token", // token
+};
+
+// load user prefs from box
+const boxConfig = $persistentStore.read("github_private_repo");
+if (boxConfig) {
+  config = JSON.parse(boxConfig);
+}
+
+const username = $request.url.match(
+  /https:\/\/(?:raw|gist)\.githubusercontent\.com\/([^\/]+)\//
+)[1];
+// rewrite headers for specific user
+if (username == config.username) {
+  console.log(`ACCESSING PRIVATE REPO: ${$request.url}`);
+  $done({ headers: {...$request.headers, Authorization: `token ${config.token}`} });
+} else $done({});
